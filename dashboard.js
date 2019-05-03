18d8ce89c4741ded13b6b19b52b3d96f0aa82617
Rizki <43452155+rizkiramdandi@users.noreply.github.com>
2019-05-03 20:44:26 +0700

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,6 @@
+{
+"name" : "devtamplate",
+"email" : "rizqiramdani3@email.com",
+"Password" : "11011999rR",
+"Description" : "Teruslah belajar dan berkarya karna kata tersebut memiliki makna tanpa batas."
+}
